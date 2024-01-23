2ec356cd5c5a41bbbabe0bd4fe59b98ffdeb667f
Yoshinori Kanno <yoshinori.kanno@me.com>
2024-01-23 12:26:38 +0900

    Delete contact route component

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,13 +1,19 @@
-import { json } from "@remix-run/node";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
 
 import { getContact } from "../data";
 
-export const loader = async ({ params }) => {
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 };
-
 
 export default function Contact() {
   const { contact } = useLoaderData<typeof loader>();
